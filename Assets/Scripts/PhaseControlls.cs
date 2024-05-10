6ffdd52e7917854c72fd3e2302c1bac35d1cd961
esdrasgc <esdrasgcdev@gmail.com>
2024-05-10 19:22:16 -0300

    cenoura v1

---
+++ Assets/Scripts/PhaseControlls.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseControlls : MonoBehaviour
+{
+    [SerializeField ] public float tempoAteACenouraEstarAtiva;
+    [SerializeField ] public float tempoParaDesativarCenoura;
+}
