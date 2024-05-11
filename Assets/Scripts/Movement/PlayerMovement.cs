4c0c9063c63490cf9eb6f2f1d897a447a6fbb905
esdrasgc <esdrasgcdev@gmail.com>
2024-05-11 20:51:46 -0300

    desisti do movimento, boa sorte ao proximo

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -1,112 +1,6 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public enum Direction
-// {
-//     North, South, East, West
-// }
-
-// public class PlayerMovement : MonoBehaviour
-// {
-//     [SerializeField] LayerMask obstacleMask;
-//     [SerializeField] bool movingHorizontally = false, canCheck = true;
-//     public float speed;
-//     Rigidbody2D rb;
-
-//     private Vector2 startTouchPosition;
-//     private bool swipeRegistered = false;
-
-//     public Direction movingDir;
-    
-//     void Start()
-//     {
-//         rb = GetComponent<Rigidbody2D>();
-//     }
-
-//     void Update()
-//     {
-//         if (Input.touchCount > 0)
-//         {
-//             Touch touch = Input.GetTouch(0);
-
-//             switch (touch.phase)
-//             {
-//                 case TouchPhase.Began:
-//                     startTouchPosition = touch.position;
-//                     swipeRegistered = false;
-//                     break;
-
-//                 case TouchPhase.Moved:
-//                     if (!swipeRegistered) // Check if swipe direction is not yet registered
-//                     {
-//                         Vector2 touchEndPosition = touch.position;
-//                         Vector2 direction = touchEndPosition - startTouchPosition;
-
-//                         if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-//                         {
-//                             // Horizontal swipe
-//                             movingHorizontally = true;
-//                             canCheck = !Physics2D.Raycast(transform.position, direction.x > 0 ? Vector2.right : Vector2.left, 1.0f, obstacleMask);
-//                             movingDir = direction.x > 0 ? Direction.East : Direction.West;
-//                         }
-//                         else
-//                         {
-//                             // Vertical swipe
-//                             movingHorizontally = false;
-//                             canCheck = !Physics2D.Raycast(transform.position, direction.y > 0 ? Vector2.up : Vector2.down, 1.0f, obstacleMask);
-//                             movingDir = direction.y > 0 ? Direction.North : Direction.South;
-//                         }
-
-//                         swipeRegistered = true; // Mark swipe as registered
-//                     }
-//                     break;
-//             }
-//         }
-
-//         // Apply constraints based on swipe direction
-//         if (canCheck)
-//         {
-//             if (movingHorizontally)
-//             {
-//                 rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-//             }
-//             else
-//             {
-//                 rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-//             }
-//         }
-//     }
-
-//     void FixedUpdate()
-//     {
-//         if (canCheck)
-//         {
-//             Vector2 moveDirection = Vector2.zero;
-//             switch (movingDir)
-//             {
-//                 case Direction.North:
-//                     moveDirection = new Vector2(0, speed * Time.fixedDeltaTime);
-//                     break;
-//                 case Direction.South:
-//                     moveDirection = new Vector2(0, -speed * Time.fixedDeltaTime);
-//                     break;
-//                 case Direction.East:
-//                     moveDirection = new Vector2(speed * Time.fixedDeltaTime, 0);
-//                     break;
-//                 case Direction.West:
-//                     moveDirection = new Vector2(-speed * Time.fixedDeltaTime, 0);
-//                     break;
-//             }
-//             rb.velocity = moveDirection;
-//         }
-//     }
-// }
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 
 public enum Direction
 {
@@ -115,97 +9,211 @@ public enum Direction
 
 public class PlayerMovement : MonoBehaviour
 {
-    // [SerializeField] LayerMask obstacleMask;
+    [SerializeField] LayerMask obstacleMask;
+    [SerializeField] bool movingHorizontally = false, canCheck = true;
     public float speed;
     Rigidbody2D rb;
 
-    public bool playerIsMoving;
-
-    public int contador_colisao = 0;
-    public int contador_saiu_colisao = 0;
+    private Vector2 startTouchPosition;
+    private bool swipeRegistered = false;
 
     public Direction movingDir;
-    // Start is called before the first frame update
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerIsMoving = true;
-        
     }
 
-    private void OnCollisionEnter2D(Collision2D other) {
-        // check if the player is colliding with an object with the obstacle layer mask
-        if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle")){
-            Debug.Log("Entrou na colisão com obstaculo");
-            playerIsMoving = false;
-            contador_colisao++;
-        }
-        
-    }
-
-    private void OnCollisionExit2D(Collision2D other) {
-         Debug.Log(other.gameObject.layer);
-         Debug.Log(LayerMask.NameToLayer("Obstacle"));
-        if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle")){
-            Debug.Log("Saiu da colisão com obstaculo");
-            playerIsMoving = true;
-            contador_saiu_colisao++;
-        }
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        if (playerIsMoving) return;
-        
-        if (Input.GetAxisRaw("Horizontal") != 0){
-            rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
-            if (Input.GetAxisRaw("Horizontal") > 0)
-            {
-                movingDir = Direction.East;
-            }
-            else
-            {
-                movingDir = Direction.West;
-            }
-            DefineVelocity();
-        }
-        else if (Input.GetAxisRaw("Vertical") != 0){
-            rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
-            if (Input.GetAxisRaw("Vertical") > 0)
-            {
-                movingDir = Direction.North;
-            }
-            else
-            {
-                movingDir = Direction.South;
-            }
-            DefineVelocity();
-        }
-    
-        
-    }
-
-    void DefineVelocity(){
-        switch (movingDir)
+        if (Input.touchCount > 0)
         {
-            case Direction.North:
-                rb.velocity = new Vector2(0, speed * Time.fixedDeltaTime);
-                break;
-            case Direction.South:
-                rb.velocity = new Vector2(0, -speed*Time.fixedDeltaTime);
-                break;
-            case Direction.East:
-                rb.velocity = new Vector2(speed*Time.fixedDeltaTime, 0);
-                break;
-            case Direction.West:
-                rb.velocity = new Vector2(-speed*Time.fixedDeltaTime, 0);
-                break;
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    startTouchPosition = touch.position;
+                    swipeRegistered = false;
+                    break;
+
+                case TouchPhase.Moved:
+                    if (!swipeRegistered) // Check if swipe direction is not yet registered
+                    {
+                        Vector2 touchEndPosition = touch.position;
+                        Vector2 direction = touchEndPosition - startTouchPosition;
+
+                        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                        {
+                            // Horizontal swipe
+                            movingHorizontally = true;
+                            canCheck = !Physics2D.Raycast(transform.position, direction.x > 0 ? Vector2.right : Vector2.left, 1.0f, obstacleMask);
+                            movingDir = direction.x > 0 ? Direction.East : Direction.West;
+                        }
+                        else
+                        {
+                            // Vertical swipe
+                            movingHorizontally = false;
+                            canCheck = !Physics2D.Raycast(transform.position, direction.y > 0 ? Vector2.up : Vector2.down, 1.0f, obstacleMask);
+                            movingDir = direction.y > 0 ? Direction.North : Direction.South;
+                        }
+
+                        swipeRegistered = true; // Mark swipe as registered
+                    }
+                    break;
+            }
+        }
+
+        // Apply constraints based on swipe direction
+        if (canCheck)
+        {
+            if (movingHorizontally)
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+            }
+            else
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+            }
         }
     }
 
-    void FixedUpdate(){
-        if (!playerIsMoving) return;
-        DefineVelocity();
+    void FixedUpdate()
+    {
+        if (canCheck)
+        {
+            Vector2 moveDirection = Vector2.zero;
+            switch (movingDir)
+            {
+                case Direction.North:
+                    moveDirection = new Vector2(0, speed * Time.fixedDeltaTime);
+                    break;
+                case Direction.South:
+                    moveDirection = new Vector2(0, -speed * Time.fixedDeltaTime);
+                    break;
+                case Direction.East:
+                    moveDirection = new Vector2(speed * Time.fixedDeltaTime, 0);
+                    break;
+                case Direction.West:
+                    moveDirection = new Vector2(-speed * Time.fixedDeltaTime, 0);
+                    break;
+            }
+            rb.velocity = moveDirection;
+        }
     }
 }
+
+
+
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+
+// public enum Direction
+// {
+//     North, South, East, West, None
+// }
+
+// public class PlayerMovement : MonoBehaviour
+// {
+//     // [SerializeField] LayerMask obstacleMask;
+//     public float speed;
+//     Rigidbody2D rb;
+
+//     public bool playerIsMoving;
+
+//     public int contador_colisao = 0;
+//     public int contador_saiu_colisao = 0;
+
+//     public Direction movingDir;
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//         playerIsMoving = true;
+        
+//     }
+
+//     private void OnCollisionEnter2D(Collision2D other) {
+//         // check if the player is colliding with an object with the obstacle layer mask
+//         if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle")){
+//             Debug.Log("Entrou na colisão com obstaculo");
+//             playerIsMoving = false;
+//             contador_colisao++;
+//             movingDir = Direction.None;
+//         }
+        
+//     }
+
+//     private void OnCollisionExit2D(Collision2D other) {
+//          Debug.Log(other.gameObject.layer);
+//          Debug.Log(LayerMask.NameToLayer("Obstacle"));
+//         if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle")){
+//             Debug.Log("Saiu da colisão com obstaculo");
+//             playerIsMoving = true;
+//             contador_saiu_colisao++;
+//         }
+//     }
+
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (playerIsMoving) return;
+        
+//         if (Input.GetAxisRaw("Horizontal") != 0){
+//             rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+//             if (Input.GetAxisRaw("Horizontal") > 0)
+//             {
+//                 movingDir = Direction.East;
+//             }
+//             else
+//             {
+//                 movingDir = Direction.West;
+//             }
+//             DefineVelocity();
+//         }
+//         else if (Input.GetAxisRaw("Vertical") != 0){
+//             rb.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation;
+//             if (Input.GetAxisRaw("Vertical") > 0)
+//             {
+//                 movingDir = Direction.North;
+//             }
+//             else
+//             {
+//                 movingDir = Direction.South;
+//             }
+//             DefineVelocity();
+//         }
+    
+        
+//     }
+
+//     void DefineVelocity(){
+//         switch (movingDir)
+//         {
+//             case Direction.None:
+//                 rb.velocity = new Vector2(0, 0);
+//                 break;
+//             case Direction.North:
+//                 rb.velocity = new Vector2(0, speed * Time.fixedDeltaTime);
+//                 break;
+//             case Direction.South:
+//                 rb.velocity = new Vector2(0, -speed*Time.fixedDeltaTime);
+//                 break;
+//             case Direction.East:
+//                 rb.velocity = new Vector2(speed*Time.fixedDeltaTime, 0);
+//                 break;
+//             case Direction.West:
+//                 rb.velocity = new Vector2(-speed*Time.fixedDeltaTime, 0);
+//                 break;
+//         }
+//     }
+
+//     void FixedUpdate(){
+//         if (!playerIsMoving) return;
+//         DefineVelocity();
+//     }
+// }
