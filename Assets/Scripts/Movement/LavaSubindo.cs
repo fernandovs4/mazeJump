6d6dae07133f9dc1fcaa50f755d92c1a5fbcf69d
esdrasgc <esdrasgcdev@gmail.com>
2024-05-18 22:56:40 -0300

    Lava

---
+++ Assets/Scripts/Movement/LavaSubindo.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaSubindo : MonoBehaviour
+{
+    public float velocidade = 1f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * velocidade * Time.deltaTime);
+    }
+}
