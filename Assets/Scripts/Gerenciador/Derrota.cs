8d08665296f316a1bd355d77959eaa3095ec2a3f
fernandovs4 <fervsantos3000@gmail.com>
2024-05-28 15:43:24 -0300

    fases novas

---
+++ Assets/Scripts/Gerenciador/Derrota.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Derrota : MonoBehaviour
+{
+
+    public void Start(){
+        PlayerPrefs.SetInt("CurrentCoins", 0);
+        PlayerPrefs.Save();
+    }
+    public void MenuLevel(){
+        SceneManager.LoadScene("LevelMenu_Scroll");
+    }
+
+
+    public void RestartLevel(){
+        int currentLevel = PlayerPrefs.GetInt("CurrentLevel", 1);
+        SceneManager.LoadScene("jogoFase" + currentLevel);
+    }
+}
